92efe42c2801a4c834e4622f9d709482abdbe423
Marc Brooks <IDisposable@gmail.com>
2024-08-21 18:31:14 -0500

    Capture slotted elements child nodes
    When the element's  has no assignedNodes, fall-back to the node's childNodes for ShadowSlotElements.
    Thanks @cWenyu
    Fixes #178, Merges #179

--- src/dom-to-image-more.js
+++ src/dom-to-image-more.js
@@ -441,7 +441,10 @@
 
             function getRenderedChildren(original) {
                 if (util.isShadowSlotElement(original)) {
-                    return original.assignedNodes(); // shadow DOM <slot> has "assigned nodes" as rendered children
+                    const assignedNodes = original.assignedNodes();
+                    
+                    if (assignedNodes && assignedNodes.length() > 0)
+                        return assignedNodes; // shadow DOM <slot> has "assigned nodes" as rendered children
                 }
                 return original.childNodes;
             }
@@ -663,7 +666,7 @@
         function isInShadowRoot(value) {
             return (
                 value !== null &&
-                Object.prototype.hasOwnProperty.call(value, 'getRootNode') &&
+                'getRootNode' in value &&
                 isShadowRoot(value.getRootNode())
             );
         }
@@ -1121,12 +1124,7 @@
             function getCssRules(styleSheets) {
                 const cssRules = [];
                 styleSheets.forEach(function (sheet) {
-                    if (
-                        Object.prototype.hasOwnProperty.call(
-                            Object.getPrototypeOf(sheet),
-                            'cssRules'
-                        )
-                    ) {
+                    if ('cssRules' in Object.getPrototypeOf(sheet)) {
                         try {
                             util.asArray(sheet.cssRules || []).forEach(
                                 cssRules.push.bind(cssRules)
