2fcc0fdfe33a7777568242f97fbb31f53505f28c
Marc Brooks <IDisposable@gmail.com>
2024-08-21 18:18:41 -0500

    Emit valid SVG in test jig
    Need to escape the emitted SVG in the debug output so we can just save as HTML and see how things looked different.

--- spec/dom-to-image-more.spec.js
+++ spec/dom-to-image-more.spec.js
@@ -273,11 +273,7 @@
 
             it('should render web fonts', function (done) {
                 this.timeout(5000);
-                loadTestPage(
-                    'fonts/dom-node.html',
-                    'fonts/style.css',
-                    'fonts/control-image'
-                )
+                loadTestPage('fonts/dom-node.html','fonts/style.css', 'fonts/control-image')
                     .then(renderToPngAndCheck)
                     .then(done)
                     .catch(done);
@@ -571,10 +567,12 @@
                 const controlUrl = getImageDataURL(controlImage(), 'image/png');
 
                 if (imageUrl !== controlUrl) {
+                    var escapedImage = escapeImage(image.src);
+
                     console.debug(`
                     <html>
                         <body>
-                            <h2>Source</h2>\n<img src='${image.src}'/>
+                            <h2>Source</h2>\n<img src='${escapedImage}'/>
                             <h2>Output</h2>\n<img src='${imageUrl}'/>
                             <h2>Control</h2>\n<img src='${controlUrl}'/>
                         </body>
@@ -586,7 +584,69 @@
                     controlUrl,
                     'rendered and control images should be same'
                 );
+
+                function escapeImage(image) {
+                    if (image.indexOf('image/svg') >= 0){
+                        const svgStart = image.indexOf('<svg');
+                        const svgEnd = image.lastIndexOf('</svg>');
+                        const prefix = image.substring(0, svgStart);
+                        const postfix = image.substring(svgEnd + 6);
+                        const embeddedSvg = image.substring(svgStart, svgEnd + 6);
+                        const escapedSvg = escapeHtml(embeddedSvg);
+                        return prefix + escapedSvg + postfix;
+                    } else {
+                        return image;
+                    }
+                }
             }
+
+            const matchHtmlRegExp = /["'&<>]/;
+            function escapeHtml(string) {
+                var str = '' + string;
+                var match = matchHtmlRegExp.exec(str);
+              
+                if (!match) {
+                    return str;
+                }
+              
+                var escape;
+                var html = '';
+                var index = 0;
+                var lastIndex = 0;
+              
+                for (index = match.index; index < str.length; index++) {
+                    switch (str.charCodeAt(index)) {
+                    case 34: // "
+                        escape = '&quot;';
+                        break;
+                    case 38: // &
+                        escape = '&amp;';
+                        break;
+                    case 39: // '
+                        escape = '&#39;';
+                        break;
+                    case 60: // <
+                        escape = '&lt;';
+                        break;
+                    case 62: // >
+                        escape = '&gt;';
+                        break;
+                    default:
+                        continue;
+                    }
+              
+                    if (lastIndex !== index) {
+                        html += str.substring(lastIndex, index);
+                    }
+              
+                    lastIndex = index + 1;
+                    html += escape;
+                }
+              
+                return lastIndex !== index
+                    ? html + str.substring(lastIndex, index)
+                    : html;
+                }
 
             function getImageDataURL(image, mimetype) {
                 var canvas = document.createElement('canvas');
